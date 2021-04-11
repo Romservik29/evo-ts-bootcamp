a6be353a4aaafd217c0bea0f4fcdeef3d6d55d29
romservik29 <andrei.romservik29@gmail.com>
2021-04-11 22:24:24 +0300

    add speed change

--- homeworks/01-bubble-sort/src/App.tsx
+++ homeworks/01-bubble-sort/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import './App.css';
 import ArrayNumber from './components/ArrayNumber';
 import Controls from './components/Controls';
@@ -21,7 +21,7 @@ type State = {
 
 const BubbleSort = (): JSX.Element => <b style={{ fontSize: 50, color: "#50aedbe8" }}>∘˚˳ ° ∘ °Bubble sort∘˚˳ ° ∘ °</b>
 export class App extends Component<{}, State> {
-  constructor(props: any) {
+  constructor(props: {}) {
     super(props)
     this.state = {
       arr: { arr: [...rndArray(30, 200)]},
@@ -29,15 +29,15 @@ export class App extends Component<{}, State> {
       speed: 20
     }
   }
-  newArray = (length = 30, maxNumber = 200) => {
+  newArray = (length = 30, maxNumber = 200):void => {
     if (this.state.interval) clearInterval(this.state.interval)
     this.setState({interval: null, arr: { arr: [...rndArray(length, maxNumber)]}, sorting: Sorting.NotRuning })
   }
-  pause = () => {
+  pause = ():void => {
     if (this.state.interval) clearInterval(this.state.interval)
     this.setState({interval:null, sorting: Sorting.Paused })
   }
-  sortArr = () => {
+  sortArr = ():void => {
     const interval = setInterval(() => {
       this.setState({ arr: stepSortArr(this.state.arr) })
       if (this.state.arr.finished) {
@@ -47,18 +47,26 @@ export class App extends Component<{}, State> {
     }, this.state.speed)
     this.setState({ interval: interval, sorting: Sorting.Running })
   }
-  // changeSpeed = (speed: number) => {
-  //   setTimeout(() => {
-  //     this.setState({ interval: null, speed: speed })
-  //     this.state.sorting && this.sortArray();
-  //   })
-  // }
+  changeSpeed = (speed: number):void => {
+    if (this.state.interval) {
+      clearInterval(this.state.interval)
+    const interval = setInterval(() => {
+      this.setState({ arr: stepSortArr(this.state.arr) })
+      if (this.state.arr.finished) {
+        clearInterval(interval)
+        this.setState({interval: null, sorting: Sorting.Finished })
+      }
+    }, speed)
+    this.setState({ interval: interval, sorting: Sorting.Running, speed: speed })
+    }
+    else this.setState({speed: speed})
+  }
   render() {
     return (
       <div className="App">
         <BubbleSort />
         <ArrayNumber array={this.state.arr} />
-        <Controls newArr={this.newArray} sort={this.sortArr} pause={this.pause} sorting={this.state.sorting} speed={this.state.speed} />
+        <Controls newArr={this.newArray} sort={this.sortArr} pause={this.pause} sorting={this.state.sorting} changeSpeed={this.changeSpeed} speed={this.state.speed} />
         <span>{Sorting[this.state.sorting]}</span>
       </div>
     )
--- homeworks/01-bubble-sort/src/components/Controls.tsx
+++ homeworks/01-bubble-sort/src/components/Controls.tsx
@@ -6,7 +6,7 @@ type Props = {
     sort: () => void
     sorting: Sorting | null
     speed: number
-    changeSpeed?: (speed: number) => void
+    changeSpeed: (speed: number) => void
     pause: () => void
 }
 
@@ -14,10 +14,10 @@ export default class Controls extends Component<Props, { speed: number }> {
     state = {
         speed: 20
     }
-    // handleChangeSpeed = (e:React.ChangeEvent<HTMLInputElement>) => {
-    //         this.setState({ speed: +e.target.value})    
-    //         this.props.changeSpeed(30)    
-    // }
+    handleChangeSpeed = (e:React.ChangeEvent<HTMLInputElement>) => {
+            this.setState({ speed: +e.target.value})    
+            this.props.changeSpeed(+e.target.value)    
+    }
     render() {
         return (
             <div>
@@ -26,7 +26,7 @@ export default class Controls extends Component<Props, { speed: number }> {
                 {this.props.sorting === Sorting.NotRuning ? <button onClick={() => this.props.sort()}>Sort</button> : null}
                 {this.props.sorting === Sorting.Paused ? <button onClick={() => this.props.sort()}>Resume</button> : null}
                 <button onClick={() => this.props.newArr()}>Set new array</button>
-                {/* <input type="range" max="1000" min="10" step="10" value={this.state.speed} onChange={(e) => this.handleChangeSpeed(e)}/><label>{this.state.speed}</label> */}
+                <input type="range" max="1000" min="10" step="10" value={this.state.speed} onChange={(e) => this.handleChangeSpeed(e)}/><label>{this.state.speed}</label>
             </div>
         )
     }
